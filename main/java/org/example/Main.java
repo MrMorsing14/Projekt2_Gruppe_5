7c13edfb32b027b5b0eb69d6fdd008aec6cf0314
MrMorsing14 <frmo0006@stud.kea.dk>
2024-11-21 23:11:35 +0100

    Delete main/java/org/example directory

--- main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-import java.time.LocalDate;
-public class Main {
-    public static void main(String[] args) {
-        FileHandler json = new FileHandler();
-        json.saveMedlem(new Medlem("Frederik", LocalDate.of(2000, 12, 30), "Aktiv", "Kredit"));
-        json.jsonReader();
-        }
-}
